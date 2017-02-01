c40649e1518086836b46277e4c6aa3a5a5e2828a
Nagarajan SD <nagarajan.sd@jifflenow.com>
2017-02-01 08:48:01 +0530

    Populated Home Controller Data From DB

--- Kelu/Viewcontroller/HomeViewController.m
+++ Kelu/Viewcontroller/HomeViewController.m
@@ -9,16 +9,15 @@
 #import "HomeViewController.h"
 #import "TextsResponse.h"
 #import "HomeTableView.h"
+#import "TextThemeTable.h"
 
 @interface HomeViewController ()
 {
-    NSIndexPath *selectedIndexPath;
-    TextsResponse *textsResponse;
+    NSArray *textModels;
 }
 
 @property (weak, nonatomic) IBOutlet HomeTableView *homeTableView;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
-@property (nonatomic, strong) NSArray *arrPeopleInfo;
 
 @end
 
@@ -42,7 +41,8 @@
     if (self.refreshRequired)
     {
         self.refreshRequired = NO;
-        [self fetchDataForHome];
+        [self fetchTextModelFromDB];
+        //[self fetchDataForHome];
     }
 }
 
@@ -81,7 +81,8 @@
         withSuccessCompletionBlock:^(NSString *responseString) {
             
             [KeluActivityIndicator hideIndicatorForView:self.view animated:YES];
-            textsResponse = [[TextsResponse alloc] initWithString:responseString error:nil];
+            TextsResponse *textsResponse = [[TextsResponse alloc] initWithString:responseString error:nil];
+            textModels = textsResponse.TextsResponse;
             [self reload];
             
         } withFailureCompletionBlock:^(NSError *error) {
@@ -89,6 +90,14 @@
             [KeluActivityIndicator hideIndicatorForView:self.view animated:YES];
             [KeluAlertViewController showAlertControllerWithTitle:@"Error"                                                                                                 message:error.localizedDescription acceptActionTitle:@"OK"                                    acceptActionBlock:nil dismissActionTitle:nil dismissActionBlock:nil presentingViewController:self];
     }];
+}
+
+- (void)fetchTextModelFromDB
+{
+        textModels = [TextThemeTable getTextModelForThemeTag:[KKeyChain loadKeyChainValueForKey:kKeychainSelectedThemeTag]
+                                        withSelectedLanguage:[KKeyChain loadKeyChainValueForKey:kKeychainSelectedLanguageKey]
+                                                   dbManager:self.dbManager];
+    [self reload];
 }
 
 #pragma mark - Parameters
@@ -104,7 +113,7 @@
 
 - (void)reload
 {
-    self.homeTableView.textResponse = textsResponse.TextsResponse;
+    self.homeTableView.textResponse = textModels;
 }
 
 @end
